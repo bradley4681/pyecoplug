e507920e541cce454f0393c1667d273125d150ba
bradley4681 <bradleysessions@gmail.com>
2023-05-30 14:27:04 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='pyecoplug',
         version='0.0.5',
         description='Interface for the EcoPlug outlet',
-        url='http://github.com/philburr/pyecoplug',
+        url='http://github.com/bradley4681/pyecoplug',
         author='phil.burr@gmail.com',
         license='MIT',
         packages=['pyecoplug'],
