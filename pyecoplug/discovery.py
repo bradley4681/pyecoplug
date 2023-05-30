b8f55c1399a32efbbde5b3ea005cec38c7eec6ef
bradley4681 <bradleysessions@gmail.com>
2023-05-30 13:15:22 -0400

    Update discovery.py

--- pyecoplug/discovery.py
+++ pyecoplug/discovery.py
@@ -10,7 +10,7 @@ def normalize_string(x):
     return x
 
 class EcoDiscovery(object):
-    UDP_PORT = 8900
+    UDP_PORT = 9090
 
     def __init__(self, on_add, on_remove):
         self.on_add = on_add
